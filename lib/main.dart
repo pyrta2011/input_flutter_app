e9c6ee2059d473010ce5eee856f975e644a4178c
Alexey <pyrta2011@yandex.ru>
2021-01-10 15:02:12 +0400

    add form widget and validate and testing field

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,72 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: Text("Input"));
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Home(),
+      ),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return HomeState();
+  }
+}
+
+class HomeState extends State<Home> {
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            TextFormField(
+              key: Key("formdata"),
+              decoration:
+                  InputDecoration(icon: Icon(Icons.person), labelText: 'Name'),
+              validator: (String value) {
+                if (value.contains('@')) {
+                  return 'Do not use the @ char.';
+                } else if (value.isEmpty) {
+                  return 'Please, enter your name';
+                }
+              },
+            ),
+            TextFormField(
+              key: Key("emailFormData"),
+              decoration: InputDecoration(
+                icon: Icon(Icons.email),
+                labelText: "email",
+              ),
+              validator: (String value) {
+                if (value.isEmpty) {
+                  return "Please, enter email";
+                } else if (!value.contains('@')) {
+                  return "Email, must contains '@'";
+                }
+              },
+            ),
+            RaisedButton(
+              onPressed: () {
+                if (_formKey.currentState.validate()) {
+                  Scaffold.of(context).showSnackBar(SnackBar(
+                    content: Text("Form load is success"),
+                    backgroundColor: Colors.green,
+                  ));
+                }
+              },
+              child: Text("Validate form"),
+            )
+          ],
+        ));
   }
 }
--- test/main_test.dart
+++ test/main_test.dart
@@ -15,16 +15,13 @@ void main() {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    //Testing Form field and button
+    await tester.enterText(find.byKey(Key("formdata")), "Fedya");
+    await tester.enterText(find.byKey(Key("emailFormData")), "fedya@gmail.com");
+    await tester.tap(find.byType(RaisedButton));
     await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text("Form load is success"), findsOneWidget);
   });
 }
